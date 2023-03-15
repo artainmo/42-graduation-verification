45bba4b2cb697194c74f461d590d47a0e249bbb8
artainmo <tainmontarthur@icloud.com>
2023-03-15 22:48:29 +0100

    fixes

--- scripts/graduation_verification.sh
+++ scripts/graduation_verification.sh
@@ -6,7 +6,10 @@ if [ $# -ne 2 ]; then
     exit
 fi
 
-COMMON_CORE_FINISHED=$(curl -sH "Authorization: Bearer $ACCESS_CODE" "https://api.intra.42.fr/v2/users/$LOGIN/projects_users" | jq '.[] | select(.project.name=="ft_transcendence")."validated?"' 2>/dev/null)
+#curl -sH "Authorization: Bearer $ACCESS_CODE" -g "https://api.intra.42.fr/v2/users/$LOGIN/projects_users?page[size]=100"
+#exit
+
+COMMON_CORE_FINISHED=$(curl -sH "Authorization: Bearer $ACCESS_CODE" -g "https://api.intra.42.fr/v2/users/$LOGIN/projects_users?page[size]=100" | jq '.[] | select(.project.name=="ft_transcendence")."validated?"' 2>/dev/null)
 
 if [ $? -ne 0 ]; then
 	echo "API call failed, maybe the access code is wrong or outdated."
@@ -32,7 +35,7 @@ check_category () {
 	printf "\e[33m\e[4m$TITLE\n\e[0m"
 	for ((i = 0; i < ${#PROJECTS[@]}; i+=2)); do
     while true; do
-		    PROJECT_FINISHED=$(curl -sH "Authorization: Bearer $ACCESS_CODE" "https://api.intra.42.fr/v2/users/$LOGIN/projects_users" | jq --arg PROJECT "${PROJECTS[$i]}" '.[] | select(.project.name==$PROJECT)."validated?"' 2>/dev/null)
+		    PROJECT_FINISHED=$(curl -sH "Authorization: Bearer $ACCESS_CODE" -g "https://api.intra.42.fr/v2/users/$LOGIN/projects_users?page[size]=100" | jq --arg PROJECT "${PROJECTS[$i]}" '.[] | select(.project.name==$PROJECT)."validated?"' 2>/dev/null)
         if [ $? -eq 0 ]; then
           break
         fi
@@ -44,7 +47,7 @@ check_category () {
 			printf " * \e[31m ${PROJECTS[$i]} - 0/${PROJECT_XP}xp \xE2\x9D\x8C  \e[0m \n"
 		else
       while true; do
-  		    PROJECT_MARK=$(curl -sH "Authorization: Bearer $ACCESS_CODE" "https://api.intra.42.fr/v2/users/$LOGIN/projects_users" | jq --arg PROJECT "${PROJECTS[$i]}" '.[] | select(.project.name==$PROJECT)."final_mark"' 2>/dev/null)
+  		    PROJECT_MARK=$(curl -sH "Authorization: Bearer $ACCESS_CODE" -g "https://api.intra.42.fr/v2/users/$LOGIN/projects_users?page[size]=100" | jq --arg PROJECT "${PROJECTS[$i]}" '.[] | select(.project.name==$PROJECT)."final_mark"' 2>/dev/null)
           if [ $? -eq 0 ]; then
             break
           fi
@@ -61,7 +64,7 @@ check_category () {
 	if [ $MINIMUM_XP -gt $TOTAL_XP ]; then
 		printf "=> TOTAL XP is \e[31m$TOTAL_XP\e[0m XP with minimum being \e[31m$MINIMUM_XP\e[0m XP, you are still missing \e[31m$(($MINIMUM_XP-$TOTAL_XP))\e[0m XP \n"
 	elif [ $MINIMUM_XP -ne 0 ]; then
-		printf "=> TOTAL XP is \e[32m$TOTAL_XP\e[0m  XP with minimum being \e[32m$MINIMUM_XP\e[0m  XP\n"
+		printf "=> TOTAL XP is \e[32m$TOTAL_XP\e[0m XP with minimum being \e[32m$MINIMUM_XP\e[0m XP\n"
 	fi
 
 	if [ $MINIMUM_VALIDATED_PROJECTS -gt $TOTAL_VALIDATED_PROJECTS ]; then
@@ -83,11 +86,11 @@ printf '\e[35m***BACHELOR CRITERIA: Internships***\e[0m\n'
 TITLE='Internships'
 MINIMUM_XP=0
 MINIMUM_VALIDATED_PROJECTS=2
-PROJECTS=('startup internship' 42000
-'internship II' 63000
-'internship I' 42000
-'part_time I' 42000
-'part_time II' 63000)
+PROJECTS=('Startup Internship - Tutor Final Evaluation' 42000
+'internship I - Company Final Evaluation' 63000
+'internship II - Company Final Evaluation' 42000
+'Part_Time I Company Final Evaluation' 42000
+'Part_Time II - Company Final Evaluation' 63000)
 check_category "$TITLE" "$MINIMUM_XP" "$MINIMUM_VALIDATED_PROJECTS" "${PROJECTS[@]}"
 bachelor_internships_validated=$?
 
@@ -222,7 +225,7 @@ MINIMUM_XP=0
 MINIMUM_VALIDATED_PROJECTS=1
 PROJECTS=('42sh' 15750
 'doom-nukem' 15750
-'inception-of-things' 25450
+'Inception-of-Things' 25450
 'humangl' 4200
 'kfs-2' 15750
 'override' 35700
@@ -275,6 +278,8 @@ else
   bachelor_validated=0
 fi
 
+printf "\n\e[2mFor bachelor you need to be present at 10 informative events. If you do not know how to see your attended events. In this mobile app (https://github.com/artainmo/swifty-companion) a listing of attended events is displayed.\e[0m\n\n"
+
 read -p 'Do you want to verify the master too? (y/n): ' input
 if [ $input != 'y' ]; then
 	exit 0
@@ -311,7 +316,7 @@ TITLE='System administration'
 MINIMUM_XP=50000
 MINIMUM_VALIDATED_PROJECTS=0
 PROJECTS=('taskmaster' 9450
-'inception-of-things' 25450
+'Inception-of-Things' 25450
 'cloud-1' 9450
 'ft_ping' 4200
 'ft_traceroute' 4200
@@ -413,4 +418,5 @@ if [ $bachelor_validated -eq "1" ] && [ $master_option_validated -eq "1" ]; then
 else
 	printf "=> \e[31mFAILED\e[0m\n"
 fi
-printf "If you do not know how to see your attended events. In this mobile app (https://github.com/artainmo/swifty-companion) a listing of attended events is displayed."
+
+printf "\n\e[2mFor master you need to be present at 15 informative events. If you do not know how to see your attended events. In this mobile app (https://github.com/artainmo/swifty-companion) a listing of attended events is displayed.\e[0m\n\n"
